ca94b9f803549b8362131ad4688393aa763123ab
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-29 17:38:02 -0300

    Update lisp.sh

--- lisp.sh
+++ lisp.sh
@@ -3,21 +3,21 @@
 FORMS="$*"
 
 try() {
-    if type $1 > /dev/null 2>&1; then
-	$1 $2 "(progn $FORMS)"
+     type $1 > /dev/null; 
+             $1 $2 "(progn $FORMS)"
 	exit $?
-    fi
+    
 }
 
-try sbcl "--noinform --eval"
-try clisp "-q -x"
-try ecl "-eval"
-try ccl "--eval"
-try dx86cl "--eval"
-try lx86cl "--eval"
-try wx86cl "--eval"
-try wx86cl64 "--eval"
-try gcl "-eval"
+ sbcl "--noinform --eval"
+ clisp "-q -x"
+ ecl "-eval"
+ ccl "--eval"
+ dx86cl "--eval"
+ lx86cl "--eval"
+ wx86cl "--eval"
+ wx86cl64 "--eval"
+ gcl "-eval"
 
 echo No Lisp found.
 exit 1
