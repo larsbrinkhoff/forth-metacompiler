d90255fd954d9d2a0bd25f8e96bfa8c8387583aa
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-29 18:00:27 -0300

    Update and rename lisp.sh to lisp.ts

---
+++ lisp.ts
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+FORMS="$*"
+
+try() {
+      type $1 > /dev/null; 
+	$1 $2 "(progn $FORMS)"
+	exit $?
+    
+}
+
+ sbcl "--noinform --eval"
+ clisp "-q -x"
+ ecl "-eval"
+ ccl "--eval"
+ dx86cl "--eval"
+ lx86cl "--eval"
+ wx86cl "--eval"
+ wx86cl64 "--eval"
+ gcl "-eval"
+
+ No Lisp found.
+
